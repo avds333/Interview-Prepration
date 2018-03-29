16850b4f530333158e0341d0f5c252c422b300ff
vibhordubey333 <vibhordubey333@gmail.com>
2018-03-29 11:03:43 +0530

    Update Tree.java

--- Data Structure/Tree/Tree.java
+++ Data Structure/Tree/Tree.java
@@ -1,1 +1,19 @@
-import java.io.
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Node
+{
+    int data;
+    Node left , right ; 
+    Node( int data ) 
+    {
+      this.data = data ;
+    }
+}
+class Tree
+{
+    public static void main(String ... a)
+    {
+        Tree t = new Tree() ; 
+        t.logic() ;
+    }
+}
